e2d5834c780f1fc0d05aad32770a42d7cccb7669
Dmitro Oleynik <dmjtrooleinik@gmail.com>
2025-03-27 18:10:48 +0200

    update

--- src/data/handlers.py
+++ src/data/handlers.py
@@ -1,12 +1,12 @@
 import asyncio
-
+from supabase import AsyncClient
 from database import get_async_client
 from src.schemas import User
 
 
 class DatabaseHandlers:
     """Клас для зручної роботи з хендлерами і уникненю пошуку роботи функції."""
-    def __init__(self, async_client):
+    def __init__(self, async_client: AsyncClient):
         """Ініціалізація об'єкту класу і отримання з'єднання з БД"""
         self.async_client = async_client
 
@@ -29,6 +29,10 @@ class DatabaseHandlers:
          шляхом витягування захешованого пароля з БД і порівняння з введеним щойно користувачем"""
         response = await self.async_client.table("users").select("password").eq("email", email).execute()
         return response.data[0]["password"]
+
+    # async def delete_user_by_email(self, email: str):
+    #     """Функція для видалення користувача по ел. пошті."""
+    #     response = await self.async_client.table("users").delete().eq("email", email).execute()
 
 
 a_client = asyncio.run(get_async_client())
