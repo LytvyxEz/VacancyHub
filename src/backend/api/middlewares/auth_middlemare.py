a6c327e52ae8c0aa2f6a3e0ed0083332ac31a44d
LytvyxEz <maxlytw@gmail.com>
2025-04-15 17:17:26 +0300

    www

--- src/backend/api/middlewares/auth_middlemare.py
+++ src/backend/api/middlewares/auth_middlemare.py
@@ -5,27 +5,26 @@ import jwt
 
 
 async def auth_middleware(request: Request, call_next):
-    if request.url.path not in ("/parse", '/parse/search', '/parse/results',
-                                '/auth/logout/confirm', '/auth/logout', '/me'):
+    if request.url.path not in ("/parse", '/parse/results',
+                                '/auth/logout/confirm', '/auth/logout'):
         return await call_next(request)
 
     access_token = request.cookies.get("access_token")
-    response = RedirectResponse(url='/auth/refresh')
+    response = RedirectResponse(url='/error?msg=Not+Authorized')
     response.set_cookie(key="next_url",
                         value=request.url.path,
                         )
-
     try:
         if access_token:
             payload = JWT.decode_jwt(access_token)
             if payload.get('typ') != "access":
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+                return response
             request.state.user = payload["sub"]
 
     except jwt.ExpiredSignatureError:
         return response
 
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        return RedirectResponse(url=f'/error?msg={e}')
 
     return await call_next(request)
